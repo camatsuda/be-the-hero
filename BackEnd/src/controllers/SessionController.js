0bae885f5f9ffd4a4c8d8e65baa9753f965dd5d5
Carol Matsuda <carolinam.matsuda@gmail.com>
2020-03-26 15:56:03 -0300

    Backend da aplicacao

---
+++ BackEnd/src/controllers/SessionController.js
@@ -0,0 +1,14 @@
+const connection = require('../database/connection');
+
+module.exports = {
+
+    async index(request, response)  {
+        const {login} = request.body;
+
+        const ongs = await connection('ongs').select('name').where('id', login).first();
+        if (!ongs){
+           return response.status(400).json({ error: 'No ONG found with this ID' });
+        }
+        return response.json({ ongs });
+    }
+}
