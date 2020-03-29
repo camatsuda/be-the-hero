728a190aec7829b18f4df681447b02bc78c51832
Carol Matsuda <carolinam.matsuda@gmail.com>
2020-03-29 10:42:00 -0300

    Frontend da aplicação

--- BackEnd/src/controllers/SessionController.js
+++ BackEnd/src/controllers/SessionController.js
@@ -9,6 +9,6 @@ module.exports = {
         if (!ongs){
            return response.status(400).json({ error: 'No ONG found with this ID' });
         }
-        return response.json({ ongs });
+        return response.json( ongs );
     }
 }
